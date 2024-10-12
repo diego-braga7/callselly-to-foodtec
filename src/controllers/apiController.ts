1d5f689e285b86aeae5c023f412d9ee946b99701
Diego Braga <diego.braga@gmail.com>
2024-10-12 20:10:03 -0300

    melhorando o formatdo da chamada

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import axios from "axios";
 
-const apiUrl = process.env.URL_FOODTEC;
-const apiUsername = process.env.USERNAME_FOODTEC;
-const apiPassword = process.env.PASSWORD_FOODTEC;
-const apiAuthToken = Buffer.from(`${apiUsername}:${apiPassword}`).toString("base64");
+const apiUrl = "https://dedhamlab.foodtecsolutions.com";
+const apiUsername = "apiclient";
+const apiPassword = "6SWXRaX8mBe48qH";
+const apiAuthToken = Buffer.from(`${apiUsername}:${apiPassword}`).toString(
+  "base64",
+);
 export const getDataFromAnotherAPI = async (req: Request, res: Response) => {
   try {
     const response = await axios.get("https://api.exemplo.com/endpoint");
@@ -14,9 +16,7 @@ export const getDataFromAnotherAPI = async (req: Request, res: Response) => {
   }
 };
 
-
 export const MenuCategories = async (req: Request, res: Response) => {
-
   console.log(req.body);
 
   let { category, items, orderType } = req.body;
@@ -24,26 +24,36 @@ export const MenuCategories = async (req: Request, res: Response) => {
   if (orderType == undefined) {
     orderType = "Delivery";
   }
+  let baseUrl = apiUrl + `/ws/store/v1/menu/categories`;
 
-  let baseUrl = apiUrl + `/ws/store/v1/menu/categories/${category}?orderType=${orderType}`;
-  console.log(items);
-  if (items) {
-    baseUrl = apiUrl + `/ws/store/v1/menu/categories/${category}/items/${items}?orderType=${orderType}`;
+  if (category) {
+    baseUrl = baseUrl + `/${category}`;
   }
+
+  console.log(items);
+
+  if (category && items) {
+    baseUrl = baseUrl + `/items/${items}`;
+  }
+
+  baseUrl = baseUrl + `?orderType=${orderType}`;
   console.log(baseUrl);
   try {
-    const response = await axios.get(baseUrl,
-      {
-        headers: {
-          Authorization: `Basic ${apiAuthToken}`
-        }
-      }
-    );
+    const response = await axios.get(baseUrl, {
+      headers: {
+        Authorization: `Basic ${apiAuthToken}`,
+      },
+    });
     res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ message: "Erro ao acessar API externa" });
+  } catch (error: any) {
+    // Verifica se o erro tem uma resposta da API e captura a mensagem
+    const errorMessage = error.response
+      ? `Erro: ${error.response.status} - ${error.response.statusText}`
+      : error.message;
+
+    console.error("Detalhes do erro:", errorMessage);
+    res
+      .status(500)
+      .json({ message: "Erro ao acessar API externa", error: errorMessage });
   }
 };
-
-
-
